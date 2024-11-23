349644631131dc0e3ef760bc136d05043b4d2778
Hazhir <programmer4906@gmail.com>
2024-11-23 11:37:23 +0330

    .

--- src/main/java/com/example/graphql/graphql/controller/PlayerController.java
+++ src/main/java/com/example/graphql/graphql/controller/PlayerController.java
@@ -1,11 +1,15 @@
 package com.example.graphql.graphql.controller;
 
 import com.example.graphql.graphql.model.Player;
+import com.example.graphql.graphql.model.Team;
 import com.example.graphql.graphql.service.PLayerService;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class PlayerController {
@@ -20,5 +24,26 @@ public class PlayerController {
     public List<Player> getPlayers() {
         return service.getPlayers();
     }
+
+    @QueryMapping
+    public Optional<Player> getPlayer(@Argument Long id) {
+        return service.getPlayerById(id);
+    }
+
+    @MutationMapping
+    public Player createPlayer(@Argument String name, @Argument Team team) {
+        return service.add(name, team);
+    }
+
+    @MutationMapping
+    public Player updatePlayer(@Argument Long id, @Argument String name, @Argument Team team) {
+        return service.update(id, name, team);
+    }
+
+    @MutationMapping
+    public Player deletePlayer(@Argument Long id) {
+        return service.delete(id);
+    }
+
 
 }
